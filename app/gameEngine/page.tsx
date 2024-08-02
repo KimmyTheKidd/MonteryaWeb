5b18c656c0aef9f53f0002f080e4b8bdbb3a9bad
Mag1cMan <31588642+Mag1cMan@users.noreply.github.com>
2024-08-02 12:03:42 +0700

    Build 0.2.8e

--- app/gameEngine/page.tsx
+++ app/gameEngine/page.tsx
@@ -215,9 +215,11 @@ export default function Game() {
     <div className="flex items-center space-x-4 mt-4">
       <Button
         onClick={handleClickEnterFullscreen}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Enter Fullscreen
+        className="capitalize"
+        color="primary" 
+
+        >
+        Fullscreen
       </Button>
 
       <Tooltip
